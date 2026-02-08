00a38b1327a14de776aa7133e4a2dba293cd23be
Trishat-cmyk <mohapatra.trishat@gmail.com>
2026-02-08 13:14:52 +0530

    updated the source code

--- upi_validator.py
+++ upi_validator.py
@@ -5,7 +5,7 @@ def validate_upi_qr(data):
     print("[INFO] Detected UPI QR Code")
     
     if not data.startswith("upi://pay"):
-        print("[RESULT] ❌ Invalid UPI QR format\n")
+        print("[RESULT]  Invalid UPI QR format\n")
         return
 
     parsed = urlparse(data)
@@ -15,7 +15,7 @@ def validate_upi_qr(data):
 
     for field in required_fields:
         if field not in params:
-            print(f"[RESULT] ❌ Invalid UPI QR (Missing {field})\n")
+            print(f"[RESULT]  Invalid UPI QR (Missing {field})\n")
             return
 
     pa = params["pa"][0]
@@ -24,11 +24,11 @@ def validate_upi_qr(data):
     am = params.get("am", ["Not Specified"])[0]
 
     if not re.match(r"^[\w.-]+@[\w.-]+$", pa):
-        print("[RESULT] ❌ Invalid UPI ID format\n")
+        print("[RESULT]  Invalid UPI ID format\n")
         return
 
     if cu != "INR":
-        print("[RESULT] ❌ Unsupported currency\n")
+        print("[RESULT]  Unsupported currency\n")
         return
 
     print("\n✅ VALID UPI QR CODE")
